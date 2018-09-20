e4837ddda523090282217ebcb638e9c7ab54cce7
nombiezinja <tianyizhang1987@gmail.com>
2018-09-19 21:37:09 -0700

    styled this piece of shit yaya

--- app.js
+++ app.js
@@ -29,7 +29,8 @@ app.use(knexLogger(knex));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); 
 app.set('view engine', 'ejs');
-app.use('/static', express.static(path.join(__dirname, 'public')));
+app.use(express.static("public"));
+// app.use('/static', express.static(path.join(__dirname, 'public')));
 app.set('views','./public');
 
 app.use(express.json());
